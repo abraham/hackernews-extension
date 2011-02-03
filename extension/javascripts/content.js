27622c88a593d227f95d1ed1a5c93bd5edb29b0f
abraham <4braham@gmail.com>
2011-02-02 23:44:11 -0800

    implemented user profile dialogs with jquery ui

--- extension/javascripts/content.js
+++ extension/javascripts/content.js
@@ -1,32 +1,65 @@
 var hoverCard = {
-  'config': {
-    'container': $('a[href*="user?id="]')
-  },
+  config: {},
+  $users: $('a[href*="user?id="]'),  
+  friends: localStorage.getItem('friends') ? JSON.parse(localStorage.getItem('friends')) : [],
 
-  'init': function(config) {
-    if (config && typeof(config) == 'object') {
-      $.extend(hoverCard.config, config);
-    }
-    hoverCard.$container = hoverCard.config.container;
-    hoverCard.$container.attr('data-url', chrome.extension.getURL('popup.html'));
-    hoverCard.$container.tipsyHoverCard();
-    hoverCard.$container.mouseover(function(){
+  profileTemplate: '<div>\
+    <span>Created ${createdAgo} with ${karma}</span> \
+    <p>{{html about}}</p> \
+    <span><u><a href="submitted?id=${username}">submissions</a></u> <u><a href="threads?id=${username}">comments</a></u></span> \
+    </div>',
+
+  init: function (config) {
+    console.log('hoverCard.init()');
+    hoverCard.$users.live('click', function () {
       hoverCard.getUserProfile(this.text);
-    })
+      return false;
+    });
+    
+    addEventListener('unload', function () {
+      localStorage.setItem('friends', JSON.stringify(hoverCard.friends));
+    }, false);
   },
 
-  'getUserProfile': function(user) {
-    chrome.extension.sendRequest({user: user},  function(user) {
-      var html = '<table>';
-      html += '<tr><td>user:</td><td>' + user.username + '</td></tr>';
-      html += '<tr><td>created:</td><td>' + user.createdAgo + '</td></tr>';
-      html += '<tr><td>karma:</td><td>' + user.karma + '</td></tr>';
-      if (user.about) {
-        html += '<tr><td>about:</td><td>' + user.about + '</td></tr>';		
-      }
-      html += '<table>';
-      $('div#hovercard').html(html);
+  getUserProfile: function (user) {
+    console.log('hoverCard.getUserProfile()');
+    chrome.extension.sendRequest({ user: user },  function (user) {
+      $.tmpl(hoverCard.profileTemplate, user)
+        .dialog({
+          title: user.username,
+          minWidth: 600,
+          show: 'clip',
+          modal: true,
+          buttons: hoverCard.buttons(user.username)
+        });
     })
+  },
+  
+  buttons: function (username) {
+    console.log('hoverCard.buttons()');
+    var index = hoverCard.isFollowing(username),
+        action = index === -1 ? 'Follow' : 'Unfollow';
+    return [{
+      text: action,
+      click: function() {
+        var index = hoverCard.isFollowing(username),
+            action = index === -1 ? 'Follow' : 'Unfollow';
+        if (action === 'Follow') {
+          hoverCard.friends.push(username);
+        } else {
+          hoverCard.friends.splice(index, 1);
+        }
+        $(this).dialog('option', 'buttons', hoverCard.buttons(username))
+      }
+    }, {
+      text: "Close",
+      click: function() { $(this).dialog("close"); }
+    }]
+  },
+  
+  isFollowing: function (user) {
+    console.log('hoverCard.isFollowing()');
+    return hoverCard.friends.indexOf(user);
   }
 }
 
